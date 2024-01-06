e34e5009e4657459ee845638a318123077345bf8
OmerZecirovic <omer_lav@hotmail.com>
2024-01-06 16:19:15 +0100

    Popravke

--- stranica/script.js
+++ stranica/script.js
@@ -1,25 +1,33 @@
-// JavaScript code
+// Your function to open the presentation
+function openPresentation() {
+  // Replace the URL with the actual URL of your presentation or file
+  var presentationUrl = "enko prezentacija.docx";
+
+  // Open the presentation in a new browser tab
+  window.open(presentationUrl, "_blank");
+}
+
+// Your existing JavaScript code
 document.getElementById('mobile-menu').addEventListener('click', function() {
-    var navList = document.querySelector('.nav-list');
-    navList.classList.toggle('active');
-  });
-  
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  var navList = document.querySelector('.nav-list');
+  navList.classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
-  var currentLocation = location.href;
-  var navItems = document.querySelectorAll('.nav-item a');
-  
-  navItems.forEach(item => {
-    if (item.href === currentLocation) {
-      item.classList.add('active');
-    }
-  });
-  
+});
+
+var currentLocation = location.href;
+var navItems = document.querySelectorAll('.nav-item a');
+
+navItems.forEach(item => {
+  if (item.href === currentLocation) {
+    item.classList.add('active');
+  }
+});
